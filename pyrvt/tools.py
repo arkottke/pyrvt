d0df2568868804049055452d3cda20a668f33f83
Albert Kottke <albert.kottke@gmail.com>
2015-03-25 15:36:36 -0700

    Only read data from xlsx files.

--- pyrvt/tools.py
+++ pyrvt/tools.py
@@ -102,7 +102,7 @@ def read_events(fname, response_type='psa'):
     elif ext == '.xlsx':
         if openpyxl is None:
             raise RuntimeError('openpyxl is required to open an xlsx file')
-        wb = openpyxl.load_workbook(fname)
+        wb = openpyxl.load_workbook(fname, data_only=True)
         ws = wb.worksheets[0]
         rows = [[r.value for r in row] for row in ws.rows]
     else:
