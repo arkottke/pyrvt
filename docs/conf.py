f8b1064b15dd4ee260b4bfd15d77be897646905b
Albert Kottke <albert.kottke@gmail.com>
2016-01-07 08:26:24 -0800

    Changed when pyrvt is imported.

--- docs/conf.py
+++ docs/conf.py
@@ -30,12 +30,13 @@ MOCK_MODULES = ['numpy', 'numpy.testing', 'scipy', 'scipy.integrate',
 
 sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
 
+# Import pyrvt after loading the mock modules, but before setting the relative path.
+import pyrvt
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-
-import pyrvt
 
 # -- General configuration ------------------------------------------------
 
