b341c02d442c91184693c0e6f0b0edf61013c966
Albert Kottke <albert.kottke@gmail.com>
2016-03-25 11:21:58 -0700

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='pyrvt',
-    version='0.3.0',
+    version='0.4.0',
     description='Ground motion models implemented in Python.',
     long_description=readme + '\n\n' + history,
     author='Albert Kottke',
