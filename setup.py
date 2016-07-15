20a2028f58d0059ede717069c9db1ee91c50e006
Albert Kottke <albert.kottke@gmail.com>
2016-07-14 20:07:05 -0700

    Fixed version numbering.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ test_requirements = [
 
 setup(
     name='pyRVT',
-    version='0.5.5',
+    version='0.5.7',
     description='Ground motion models implemented in Python.',
     long_description=readme + '\n\n' + history,
     author='Albert Kottke',
