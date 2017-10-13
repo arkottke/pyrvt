f20802c77ed80a3d078fa1b1847207678e6f7b6a
Albert Kottke <albert.kottke@gmail.com>
2017-10-13 10:31:55 -0700

    Added numba requirement.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,18 @@
 import sys
 import os
 
+# Mock http://docs.readthedocs.io/en/latest/faq.html
 from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+
+MOCK_MODULES = ['numba']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
