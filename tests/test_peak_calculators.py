5510ef75b91b556f577992c16500acd92eb0433c
Albert Kottke <albert.kottke@gmail.com>
2018-01-15 18:26:26 -0800

    Added tests to BT15. Not sure SMSIM is working properly.

--- tests/test_peak_calculators.py
+++ tests/test_peak_calculators.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
+import pathlib
 
 import numpy as np
 from numpy.testing import assert_allclose, assert_string_equal
@@ -12,18 +12,26 @@ import pyrvt
 from . import readers
 
 
-@pytest.mark.parametrize('peak_calculator,abbrev', [
-    (pyrvt.peak_calculators.BooreJoyner1984(), 'bj84'),
-    (pyrvt.peak_calculators.LiuPezeshk1999(), 'lp99'),
-    (pyrvt.peak_calculators.BooreThompson2012('wna', 6, 20.), 'bt12_wna'),
-    (pyrvt.peak_calculators.BooreThompson2012('ena', 6, 20.), 'bt12_ena'),
+@pytest.mark.parametrize('peak_calculator,abbrev,suffix', [
+    (pyrvt.peak_calculators.BooreJoyner1984(), 'bj84', 'm6.00r020.0'),
+    (pyrvt.peak_calculators.LiuPezeshk1999(), 'lp99', 'm6.00r020.0'),
+    (pyrvt.peak_calculators.BooreThompson2012('wna', 6, 20.),
+     'bt12_wna', 'm6.00r020.0'),
+    (pyrvt.peak_calculators.BooreThompson2012('ena', 6, 20.),
+     'bt12_ena', 'm6.00r020.0'),
+    # (pyrvt.peak_calculators.BooreThompson2015('wna', 6, 21.3),
+    #  'bt15_wna', 'm6.00rps021.3'),
+    # (pyrvt.peak_calculators.BooreThompson2015('ena', 6, 20.8),
+    #  'bt15_ena', 'm6.00rps020.8'),
 ])
-def test_osc_accels(peak_calculator, abbrev):
-    path = os.path.join(os.path.dirname(__file__), 'data')
+def test_osc_accels(peak_calculator, abbrev, suffix):
+    fpath = pathlib.Path(__file__).parent / 'data'
     fs = readers.load_fourier_spectrum(
-        os.path.join(path, 'test-{}.m6.00r0020.0_fs.col'.format(abbrev)))
+        str(fpath / f'test-{abbrev}.{suffix}_fs.col')
+    )
     rs = readers.load_rvt_response_spectrum(
-        os.path.join(path, 'test-{}.m6.00r020.0_rs.rv.col'.format(abbrev)))
+        str(fpath / f'test-{abbrev}.{suffix}_rs.rv.col')
+    )
 
     rvt_motion = pyrvt.motions.RvtMotion(
         freqs=fs['freqs'],
@@ -34,7 +42,7 @@ def test_osc_accels(peak_calculator, abbrev):
     spec_accels = rvt_motion.calc_osc_accels(rs['freqs'], rs['damping'])
 
     # The SMSIM output file only provides 4 significant digits
-    assert_allclose(spec_accels, rs['spec_accels'], rtol=0.001, atol=0.05)
+    assert_allclose(spec_accels, rs['spec_accels'], rtol=0.05, atol=0.01)
 
 
 @pytest.fixture
